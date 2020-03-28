8e7d1a14d326482251775bac3fe5d8fc778904bb
cha <nakzyu@gmail.com>
2020-03-29 01:46:30 +0900

    add use Description update

---
+++ src/shared/components/UIElements/ProfileModal.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useContext } from "react";
+import { createPortal } from "react-dom";
+import "./ProfileModal.css";
+import Backdrop from "./Backdrop";
+import ImageUpload from "../FormElements/ImageUpload";
+import { AuthContext } from "../../context/auth-context";
+import { useForm } from "../../hooks/form-hook";
+import { useHttpClient } from "../../hooks/http-hook";
+
+const ModalOverlay = props => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const auth = useContext(AuthContext);
+  const [formState, inputHandler] = useForm(
+    {
+      image: {
+        value: null,
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const imageSubmitHandler = async event => {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData();
+
+      formData.append("image", formState.inputs.image.value);
+      formData.append("creator", auth.userId);
+
+      console.log(formData);
+      await sendRequest(
+        `http://localhost:5000/api/users/updateImage/${props.userId}`,
+        "PATCH",
+        formData,
+        {
+          Authorization: "Bearer " + auth.token
+        }
+      );
+    } catch (err) {}
+  };
+
+  const content = (
+    <div className="modal centered profileModal">
+      <div className="modal-wrapper">
+        <form onSubmit={imageSubmitHandler}>
+          <ImageUpload id="image" onInput={inputHandler} />
+          <button type="submit">image submit</button>
+        </form>
+      </div>
+    </div>
+  );
+
+  return createPortal(content, document.getElementById("modal-hook"));
+};
+
+const ProfileModal = props => {
+  return (
+    <Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <ModalOverlay {...props} />
+    </Fragment>
+  );
+};
+
+export default ProfileModal;
--- src/users/components/UserProfile.js
+++ src/users/components/UserProfile.js
@@ -1,24 +1,43 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
-import ImageUpload from "../../shared/components/FormElements/ImageUpload";
-import { useForm } from "../../shared/hooks/form-hook";
+import React, { useEffect, useState, useCallback, useContext } from "react";
+import Input from "../../shared/components/FormElements/Input";
 import { useHttpClient } from "../../shared/hooks/http-hook";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MAXLENGTH
+} from "../../shared/util/validators";
+
 import { AuthContext } from "../../shared/context/auth-context";
-import { useHistory } from "react-router-dom";
+import ProfileModal from "../../shared/components/UIElements/ProfileModal";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./UserProfile.css";
 
 const UserProfile = props => {
-  const history = useHistory();
   const auth = useContext(AuthContext);
+  const [showModal, setShowModal] = useState(false);
+  const [showForm, setShowForm] = useState(true);
   const [loadedUser, setLoadedUser] = useState();
+  const [formState, inputHandler] = useForm({
+    description: {
+      value: "",
+      isaValid: false
+    }
+  });
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const [formState, inputHandler] = useForm(
-    {
-      image: {
-        value: null,
-        isValid: false
-      }
-    },
-    false
-  );
+
+  const openModalHandelr = () => {
+    setShowModal(true);
+  };
+
+  const closeModalHandelr = () => {
+    setShowModal(false);
+  };
+
+  const FormHandler = () => {
+    if (showForm) {
+      descriptionSubmitHandler();
+    }
+    setShowForm(!showForm);
+  };
 
   const fetchUser = useCallback(async () => {
     try {
@@ -34,35 +53,88 @@ const UserProfile = props => {
     fetchUser();
   }, [fetchUser]);
 
-  const imageSubmitHandler = async event => {
+  const descriptionSubmitHandler = async event => {
     event.preventDefault();
-
+    console.log(formState);
     try {
-      const formData = new FormData();
-
-      formData.append("image", formState.inputs.image.value);
-      formData.append("creator", auth.userId);
-
-      console.log(formData);
-      await sendRequest(
-        `http://localhost:5000/api/users/updateImage/${props.userId}`,
+      const responseData = await sendRequest(
+        `http://localhost:5000/api/users/updateDescription/${auth.userId}`,
         "PATCH",
-        formData,
+        JSON.stringify({
+          description: formState.inputs.description.value
+        }),
         {
-          Authorization: "Bearer " + auth.token
+          Authorization: "Bearer " + auth.token,
+          "Content-Type": "application/json"
         }
       );
-      fetchUser();
-    } catch (err) {}
+      console.log(responseData);
+    } catch (err) {
+      console.log("failed");
+    }
   };
 
   return (
     <div className="user-profile">
-      {loadedUser && <img src={`${loadedUser.image}`} alt="userImage" />}
-
-      <form onSubmit={imageSubmitHandler}>
-        <ImageUpload id="image" onInput={inputHandler} />
-        <button type="submit">image submit</button>
+      <div className="user-profile_wrapper">
+        {loadedUser && (
+          <img
+            className="user-profile_image"
+            src={`${loadedUser.image}`}
+            alt="userImage"
+            onClick={openModalHandelr}
+          />
+        )}
+        {showModal && (
+          <ProfileModal
+            show={showModal}
+            onCancel={closeModalHandelr}
+            {...props}
+          />
+        )}
+        <div className="user-profile_status">
+          <div className="user-profile_nickname">
+            {loadedUser && loadedUser.name}
+          </div>
+          <div className="user-profile_posts">
+            {loadedUser && loadedUser.posts.length} post
+            {loadedUser && loadedUser.posts.length > 1 ? "s" : ""}
+          </div>
+        </div>
+      </div>
+      <form className="user-profile_description">
+        {showForm ? (
+          <Input
+            element="input"
+            id="description"
+            type="text"
+            validators={[VALIDATOR_REQUIRE(), VALIDATOR_MAXLENGTH(26)]}
+            errorText="Please enter content"
+            onInput={inputHandler}
+          />
+        ) : (
+          <div className="user-profile_description_message">
+            {loadedUser && loadedUser.description}
+          </div>
+        )}
+        {props.userId === auth.userId && (
+          <ul
+            className="user-profile_edit"
+            type="submit"
+            onClick={descriptionSubmitHandler}
+          >
+            <li>
+              <img
+                src={require("../../assets/iconmonstr-pencil-5-32.png")}
+                alt="edit"
+              />
+              <img
+                src={require("../../assets/iconmonstr-pencil-4-32.png")}
+                alt="edit"
+              />
+            </li>
+          </ul>
+        )}
       </form>
     </div>
   );
